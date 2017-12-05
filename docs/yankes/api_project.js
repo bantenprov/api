097b5d75909a0a8e7812a843d718f0b808e32cb6
Igoy Nawamreh <igoynawamreh@gmail.com>
2017-12-05 16:57:23 +0700

    use apidoc.json for url

--- docs/yankes/api_project.js
+++ docs/yankes/api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-12-05T09:51:30.843Z",
+    "time": "2017-12-05T09:55:53.282Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
