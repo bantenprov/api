fd7bf3c72742472bef451f27928194c5de9d8b79
Igoy Nawamreh <igoynawamreh@gmail.com>
2017-12-11 15:33:34 +0700

    build

--- docs/dashboard/api_project.js
+++ docs/dashboard/api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-12-11T07:34:24.870Z",
+    "time": "2017-12-11T07:47:07.203Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
--- docs/yankes/api_project.js
+++ docs/yankes/api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-12-11T07:40:50.731Z",
+    "time": "2017-12-11T07:47:31.633Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
