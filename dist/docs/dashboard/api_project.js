f840194600aa2beb94c89ea487f6d3abb536d1e4
Igoy Nawamreh <igoynawamreh@gmail.com>
2017-12-13 10:36:42 +0700

    build

--- dist/docs/dashboard/api_project.js
+++ dist/docs/dashboard/api_project.js
@@ -11,7 +11,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-12-13T03:25:19.969Z",
+    "time": "2017-12-13T03:35:57.886Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
